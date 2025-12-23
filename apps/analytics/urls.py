4830c0ede9cfeb8eb70a20743f917d10f6f61a82
amirhossein <amirhossein109020@gmail.com>
2025-12-23 12:47:52 +0330

    Implemented TopPostList/TopPostDetail APIViews.

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import DailyFollowerGrowthLogAPIView, FollowerSummaryAPIView
+from .views import (
+    DailyFollowerGrowthLogAPIView, FollowerSummaryAPIView,
+    TopPostListAPIView, TopPostDetailAPIView
+)
 
 urlpatterns = (
     path("growth-logs/", DailyFollowerGrowthLogAPIView.as_view(), name="growth_logs"),
     path("followers-summary/", FollowerSummaryAPIView.as_view(), name="followers_summary"),
+    path("top-posts/", TopPostListAPIView.as_view(), name="top_posts"),
+    path("top-posts/<int:pk>/", TopPostDetailAPIView.as_view(), name="top_posts_detail"),
 
 )
--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -2,9 +2,10 @@ from rest_framework import views, status
 from rest_framework.response import Response
 
 from apps.account.models import InstagramAccount
-from ..account.exceptions import base_response_with_error
-from .serializers import DailyFollowerGrowthLogSerializer
-from .models import DailyFollowerGrowthLog
+from apps.account.exceptions import base_response_with_error
+from apps.profiles.models import Post
+from .serializers import DailyFollowerGrowthLogSerializer, TopPostSerializer
+from .models import DailyFollowerGrowthLog, TopPosts
 from .services import AnalyticsService
 
 _analytics_svc = AnalyticsService()
@@ -24,6 +25,42 @@ class DailyFollowerGrowthLogAPIView(views.APIView):
             return base_response_with_error(str(err), status.HTTP_404_NOT_FOUND)
 
         serializer = DailyFollowerGrowthLogSerializer(logs, many=True)
+        return Response(serializer.data)
+
+
+class TopPostListAPIView(views.APIView):
+    serializer_class = TopPostSerializer
+
+    def get_queryset(self):
+        account = InstagramAccount.objects.get(user=self.request.user)
+        return TopPosts.objects.filter(account=account)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            top_posts = self.get_queryset()
+        except InstagramAccount.DoesNotExist as err:
+            return base_response_with_error(str(err), status.HTTP_404_NOT_FOUND)
+
+        serializer = TopPostSerializer(top_posts, many=True)
+        return Response(serializer.data)
+
+
+class TopPostDetailAPIView(views.APIView):
+    serializer_class = TopPostSerializer
+
+    def get_queryset(self, post_pk):
+        account = InstagramAccount.objects.get(user=self.request.user)
+        post = Post.objects.get(pk=post_pk)
+        return TopPosts.objects.get(account=account, post=post)
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            top_post = self.get_queryset(post_pk=pk)
+        except (InstagramAccount.DoesNotExist, Post.DoesNotExist, TopPosts.DoesNotExist) as err:
+            return base_response_with_error(str(err), status.HTTP_404_NOT_FOUND)
+
+        serializer = TopPostSerializer(top_post)
         return Response(serializer.data)
 
 
