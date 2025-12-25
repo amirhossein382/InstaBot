f10baafd21c679ca1b638d32ab971fd436c30ea2
amirhossein <amirhossein109020@gmail.com>
2025-12-25 12:37:09 +0330

    fix unknown method error in create_profile_periodic_tasks callback and call analyze_user_top_posts_and_best_time_to_post in create_analytics_periodic_tasks receiver callback.

--- apps/profiles/receivers.py
+++ apps/profiles/receivers.py
@@ -9,8 +9,8 @@ _logger = Logger()
 
 
 @receiver(profile_initialized)
-def create_profile_app_periodic_tasks(requests, account_id, **kwargs):
-    op = create_profile_app_periodic_tasks.__name__
+def create_profile_periodic_tasks(requests, account_id, **kwargs):
+    op = create_profile_periodic_tasks.__name__
     _logger.log_event(op, "receiver is running...")
-    _profile_svc.config.create_analyze_update_follow_data_periodic_task(account_id)
+    _profile_svc.config.create_update_profile_info_periodic_task(account_id)
     _logger.log_event(op, "receiver done!")
